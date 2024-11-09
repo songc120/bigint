d8af4ff1e1f47aad7faaec4be57905aa251f9831
Chenwei <song6479790026@gamil.com>
2024-11-09 09:46:07 -0500

    private getter setter

--- bigint.hpp
+++ bigint.hpp
@@ -4,8 +4,24 @@
 #include <iostream>
 class bigint
 {
+private:
     std::vector<uint8_t> digits;
     bool is_negative;
+
+    uint8_t get_digit(const uint64_t n) const;//
+    std::vector<uint8_t> get_digits() const;//
+
+    bigint& set_digits(std::vector<uint8_t> n);
+    bigint& push_back(const uint8_t n);//
+    bigint& pop_back();//
+    bigint& insert(std::vector<uint8_t>::iterator ind,const uint8_t num);//
+    bigint& erase(const std::vector<uint8_t>::iterator ind);//
+    size_t size() const;//
+    std::vector<uint8_t>::iterator begin();//
+    std::vector<uint8_t>::iterator end();//
+
+    bool get_is_negative() const;//
+    bigint& set_negative(bool neg);//
     // A default constructor, creating the integer 0.
     // A constructor that takes a signed 64-bit integer and converts it to an arbitrary-precision integer.
     // A constructor that takes a string of digits and converts it to an arbitrary-precision integer.
@@ -23,21 +39,6 @@ public:
     bigint(std::string n);
     bigint(const bigint &other);
     ~bigint() = default;
-
-    uint8_t get_digit(const uint64_t n) const;//
-    std::vector<uint8_t> get_digits() const;//
-
-    bigint& set_digits(std::vector<uint8_t> n);
-    bigint& push_back(const uint8_t n);//
-    bigint& pop_back();//
-    bigint& insert(std::vector<uint8_t>::iterator ind,const uint8_t num);//
-    bigint& erase(const std::vector<uint8_t>::iterator ind);//
-    size_t size() const;//
-    std::vector<uint8_t>::iterator begin();//
-    std::vector<uint8_t>::iterator end();//
-
-    bool get_is_negative() const;//
-    bigint& set_negative(bool neg);//
 
     bigint operator+(bigint const &other) const;//
     bigint& operator+=(bigint const &increment);//
